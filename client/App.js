72750b95ced92cf4d914efbde1ef816c1ebeaf25
recipely-webapp <recipelywebapp@gmail.com>
2025-06-05 19:12:11 +0200

    Add files via upload

--- client/App.js
+++ client/App.js
@@ -1,22 +1,14 @@
-import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [msg, setMsg] = useState('');
-
-  useEffect(() => {
-    fetch('http://localhost:5000/api/hello')
-      .then(res => res.json())
-      .then(data => setMsg(data.message));
-  }, []);
-
-  return (
-    <div className="App">
-      <h1>React + Express aaaaaaaaaaaaaaaaaaaa
-        
-      </h1>
-      <p>{msg}</p>
-    </div>
+  return React.createElement(
+    "div",
+    { className: "App" },
+    React.createElement(
+      "header",
+      { className: "header" },
+      "RECIPELY"
+    )
   );
 }
 
