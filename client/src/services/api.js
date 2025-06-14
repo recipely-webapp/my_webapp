d57d519ed35bdd813a761df97b6af33135eabc2f
recipely-webapp <recipelywebapp@gmail.com>
2025-06-14 19:08:41 +0200

    Feat: Aggiorna baseURL API per la produzione

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  //baseURL: 'http://localhost:5000/api',
+  baseURL:https:'https://recipely-webapp.onrender.com',
   withCredentials: true,
 });
 
